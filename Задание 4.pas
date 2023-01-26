342bf0fd9426d6470f33b21ad2c41da304c99909
伊凡 <117629350+EOn9in@users.noreply.github.com>
2023-01-26 19:48:48 +0300

    Add files via upload

---
+++ Задание 4.pas
@@ -0,0 +1,55 @@
+﻿Type
+    point=^Mylist;
+    Mylist=Record
+          Info:Integer;
+          Next:point;
+          end;
+Var
+   phead:point;
+
+Procedure Sozdanie(Var t:point);
+Var
+   y:point;
+Begin
+     New(y);
+     var i:=ReadInteger();
+     y^.Info:= i;
+     y^.Next:= t;
+     t:=y;
+     end;
+
+Procedure MaxElem(t:point);
+Var
+   y:point;
+   Max:Integer;
+Begin
+     y:=t;
+     Max:=y^.Info;
+     While y<>Nil do begin
+     If y^.Info>Max then Max:=y^.Info;
+     y:=y^.Next;
+     end;
+     println('Максимальный элемент списка: ',Max);
+end;
+
+Procedure MinElem(t:point);
+Var
+   y:point;
+   Min:Integer;
+Begin
+     y:=t;
+     Min:=y^.Info;
+     While y<>Nil do begin
+     If y^.Info<Min then Min:=y^.Info;
+     y:=y^.Next;
+     end;
+     println('Минимальный элемент списка: ',Min);
+end;
+
+Begin
+  var n:=ReadInteger('Введите кол-во элементов списка: ');
+  for var i:=1 to n do
+    Sozdanie(phead);
+  MaxElem(phead);
+  MinElem(phead);
+End.
